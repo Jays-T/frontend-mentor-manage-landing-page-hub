eeefbf1551ca7aa4fdba300710e62af5f4bb685e
John Traas <j.j.h.traas@gmail.com>
2021-01-17 00:33:39 +0100

    added mobile navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,9 +5,12 @@ $(document).ready(function() {
 });
 
 let navTog = document.querySelectorAll('#nav-toggle');
+let navList = document.getElementById('nav-links');
 
 navTog.forEach(popup => popup.addEventListener('click', (e) =>{
+    
 
     popup.classList.toggle('active');
+    navList.classList.toggle('active');
 
 }));
