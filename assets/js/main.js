8b1e5837c93eb8dede058e1c3270897a04c4342f
John Traas <j.j.h.traas@gmail.com>
2021-01-26 21:14:45 +0100

    added overlay on mobile nav shown

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,11 +6,11 @@ $(document).ready(function() {
 
 let navTog = document.querySelectorAll('#nav-toggle');
 let navList = document.getElementById('nav-links');
+const overlay = document.getElementById('overlay');
 
 navTog.forEach(popup => popup.addEventListener('click', (e) =>{
     
-
     popup.classList.toggle('active');
     navList.classList.toggle('active');
-
+    overlay.classList.toggle('overlay-switch');
 }));
