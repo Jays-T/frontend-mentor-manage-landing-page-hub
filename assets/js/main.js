90c616e1da872bd6f2cf19f690889c4d57d317c4
John Traas <j.j.h.traas@gmail.com>
2021-01-26 22:12:06 +0100

    removed bootstrap + refactored code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,10 @@
-$(document).ready(function() {
-    $('.carousel').carousel({
-        interval: 3000
-    });
-});
-
-let navTog = document.querySelectorAll('#nav-toggle');
+let navTog = document.querySelector('#nav-toggle');
 let navList = document.getElementById('nav-links');
 const overlay = document.getElementById('overlay');
 
-navTog.forEach(popup => popup.addEventListener('click', (e) =>{
+navTog.addEventListener('click', e =>{
     
-    popup.classList.toggle('active');
+    navTog.classList.toggle('active');
     navList.classList.toggle('active');
     overlay.classList.toggle('overlay-switch');
-}));
+});
